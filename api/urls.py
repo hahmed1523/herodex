028df0374bdc7ad15b4c944a946a05fe715f73cb
hahmed1523 <hahmed1523@gmail.com>
2022-10-13 16:48:09 -0400

    Add views folder

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from . import views 
+from . import view
+from .views import heroes_view
 
 urlpatterns = [ 
-    path('', views.getRoutes)
+    path('', view.getRoutes),
+    path('heroes/', heroes_view.getRoutes, name='heroes')
 ]
