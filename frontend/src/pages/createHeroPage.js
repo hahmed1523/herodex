a596173b17dd9beb54ecc177a6d2bacd0036abf8
hahmed1523 <hahmed1523@gmail.com>
2022-10-25 17:03:28 -0400

    Add form options to create Hero

--- frontend/src/pages/createHeroPage.js
+++ frontend/src/pages/createHeroPage.js
@@ -4,9 +4,27 @@ const CreateHeroPage = () => {
 
     let [hero, setHero] = useState({});
 
-    // const getFamousFrom = async () => {
-    //     let response = await fetch()
-    // }
+
+    let [heroesFrom, setHeroesFrom] = useState([]);
+    useEffect(() => {
+        getHeroesFrom();
+    }, [])
+
+    const getHeroesFrom = async () => {
+        const response = await fetch('/api/heroesfrom/');
+        const data = await response.json();
+        setHeroesFrom(data);
+    }
+
+    let [moves, setMoves] = useState([]);
+    useEffect(() => {
+        getMoves();
+    }, [])
+    const getMoves = async () => {
+        const response = await fetch('/api/moves/');
+        const data = await response.json();
+        setMoves(data);
+    }
 
     return (
         <div>
@@ -20,22 +38,32 @@ const CreateHeroPage = () => {
                 <textarea name="description"></textarea>
 
                 <label htmlFor="famous_from">Famous From:</label> 
-                <select name="famous_from">
-                    <option>Option1</option>
+                <select name="famous_from" defaultValue={"Select"}>
+                    <option disabled>Select</option>
+                    {heroesFrom?.map(heroFrom => (
+                        <option key={heroFrom.id} value={heroFrom.id}>{heroFrom.name}</option>
+                    ))}
                 </select>
-
+                
                 <label htmlFor="move1">Primary Move:</label>
-                <select name="move1">
-                    <option>Option1</option>
+                <select name="move1" defaultValue={"Select"}>
+                    <option disabled>Select</option>
+                    {moves?.map(move => (
+                        <option key={move.id} value={move.id}>{move.name}</option>
+                    ))}
                 </select>
 
-                <label htmlFor="move2">Primary Move:</label>
-                <select name="move2">
-                    <option>Option1</option>
+                <label htmlFor="move2">Secondary Move:</label>
+                <select name="move2" defaultValue={"Select"}>
+                    <option disabled>Select</option>
+                    {moves?.map(move => (
+                        <option key={move.id} value={move.id}>{move.name}</option>
+                    ))}
                 </select>
 
                 <input type='submit' value='Add'/>
             </form>
+
         </div>
     );
 };
