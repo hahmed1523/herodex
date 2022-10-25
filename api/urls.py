bbd5186d7700158b83ce77c11dc8c7dfe08236f4
hahmed1523 <hahmed1523@gmail.com>
2022-10-25 15:53:31 -0400

    Add Move Api

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path 
 from . import view
-from .views import heroes_view, hero_from_view
+from .views import heroes_view, hero_from_view, moves_view
 
 urlpatterns = [ 
     path('', view.getRoutes),
     path('heroes/', heroes_view.getHeroes, name='heroes'),
     path('heroes/<str:pk>/', heroes_view.getHeroes, name='hero'),
     path('heroesfrom/', hero_from_view.getHeroFrom, name='heroesfrom'),
-    path('heroesfrom/<str:pk>/', hero_from_view.getHeroFrom, name='herofrom')
+    path('heroesfrom/<str:pk>/', hero_from_view.getHeroFrom, name='herofrom'),
+    path('moves/', moves_view.MovesView.as_view(), name="moves"),
+    path('moves/<str:pk>/', moves_view.MovesView.as_view(), name="move")
     
 ]
---
+++ api/views/moves_view.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models.moves import Move
+from ..serializers.moves_serializer import MoveSerializer
+
+class MovesView(APIView):
+    def get(self, request, pk=None, format=None):
+        # Handle single request
+        if pk:
+            move = Move.objects.get(id=pk)
+            serializer = MoveSerializer(move, many=False)
+            return Response(serializer.data)
+        else:
+            # Handle general request
+            moves = Move.objects.all()
+            serializer = MoveSerializer(moves, many=True)
+            return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        data = request.data
+        move= Move.objects.create(
+            name = data['name'],
+            user_id = data['user']
+        )
+
+        serializer = MoveSerializer(move, many=False)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        data = request.data 
+        move= Move.objects.get(id=pk)
+        serializer = MoveSerializer(instance=move, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+        
+        return Response(serializer.data)
+    
+    def delete(self, request, pk, format=None):
+        move = Move.objects.get(id=pk)
+        move.delete()
+        return Response('Move is deleted')
+
