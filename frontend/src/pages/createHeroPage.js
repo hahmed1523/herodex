64cec19e14dda5dacc05bdf24cf877ff7a3bc6fc
hahmed1523 <hahmed1523@gmail.com>
2022-10-31 16:44:55 -0400

    Add create hero front end

--- frontend/src/pages/createHeroPage.js
+++ frontend/src/pages/createHeroPage.js
@@ -2,7 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 const CreateHeroPage = () => {
 
-    let [hero, setHero] = useState({});
+    let [hero, setHero] = useState({
+        'name': '',
+        'description': '',
+        'famous_from': null,
+        'move1': null,
+        'move2': null,
+        'user': 1
+    });
+
+    const update = field => {
+        const name = field;
+        return e => setHero({
+            ...hero,
+            [name]: e.target.value
+        })
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/api/heroes/',{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(hero)
+        });
+        const data = await response.json();
+        console.log(data);
+    };
 
 
     let [heroesFrom, setHeroesFrom] = useState([]);
@@ -30,35 +58,49 @@ const CreateHeroPage = () => {
         <div>
             <h2>Add a Hero</h2>
 
-            <form className='hero-create'>
+            <form className='hero-create'
+                onSubmit={handleSubmit}>
                 <label htmlFor="name">Name:</label>
-                <input type='text' name='name'/>
+                <input type='text' 
+                    name='name' 
+                    value={hero.name}
+                    onChange={update('name')}/>
 
                 <label htmlFor="description">Description:</label>
-                <textarea name="description"></textarea>
+                <textarea name="description"
+                    value={hero.description}
+                    onChange={update('description')}></textarea>
 
                 <label htmlFor="famous_from">Famous From:</label> 
-                <select name="famous_from" defaultValue={"Select"}>
-                    <option disabled>Select</option>
-                    {heroesFrom?.map(heroFrom => (
-                        <option key={heroFrom.id} value={heroFrom.id}>{heroFrom.name}</option>
-                    ))}
+                <select name="famous_from" 
+                    defaultValue={"Select"}
+                    onChange={update('famous_from')}>
+                        <option disabled>Select</option>
+                        {heroesFrom?.map(heroFrom => (
+                            <option key={heroFrom.id} value={heroFrom.id}>{heroFrom.name}</option>
+                        ))}
                 </select>
                 
                 <label htmlFor="move1">Primary Move:</label>
-                <select name="move1" defaultValue={"Select"}>
-                    <option disabled>Select</option>
-                    {moves?.map(move => (
-                        <option key={move.id} value={move.id}>{move.name}</option>
-                    ))}
+                <select 
+                    name="move1" 
+                    defaultValue={"Select"}
+                    onChange={update('move1')}>
+                        <option disabled>Select</option>
+                        {moves?.map(move => (
+                            <option key={move.id} value={move.id}>{move.name}</option>
+                        ))}
                 </select>
 
                 <label htmlFor="move2">Secondary Move:</label>
-                <select name="move2" defaultValue={"Select"}>
-                    <option disabled>Select</option>
-                    {moves?.map(move => (
-                        <option key={move.id} value={move.id}>{move.name}</option>
-                    ))}
+                <select 
+                    name="move2" 
+                    defaultValue={"Select"}
+                    onChange={update('move2')}>
+                        <option disabled>Select</option>
+                        {moves?.map(move => (
+                            <option key={move.id} value={move.id}>{move.name}</option>
+                        ))}
                 </select>
 
                 <input type='submit' value='Add'/>
