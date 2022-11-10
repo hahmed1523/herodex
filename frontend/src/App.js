a54f5b78a6613863a48aedd91a12f2fac5a8305a
hahmed1523 <hahmed1523@gmail.com>
2022-11-10 16:20:41 -0500

    Create subfolders in pages for hero and hero_sources and move files to them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import './styles/styles.js';
 import { Routes, Route, Link } from 'react-router-dom';
 import Header from './components/header';
 import HomePage from './pages/homePage';
-import HeroPage from './pages/heroPage.js';
-import CreateHeroPage from './pages/createHeroPage.js';
-import CreateHeroFromPage from './pages/createHeroFromPage.js';
-import HeroesFromIndexPage from './pages/heroesFromIndexPage.js';
-import HeroesFromPage from './pages/heroesFromPage.js';
+import HeroPage from './pages/hero/heroPage.js';
+import CreateHeroPage from './pages/hero/createHeroPage.js';
+import CreateHeroFromPage from './pages/hero_sources/createHeroFromPage.js';
+import HeroesFromIndexPage from './pages/hero_sources/heroesFromIndexPage.js';
+import HeroesFromPage from './pages/hero_sources/heroesFromPage.js';
 
 function App() {
   return (
--- frontend/src/pages/hero_sources/heroesFromIndexPage.js
+++ frontend/src/pages/hero_sources/heroesFromIndexPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SourceItem from '../components/heroesFromItem';
+import SourceItem from '../../components/heroesFromItem';
 
 const HeroesFromIndexPage = () => {
 
