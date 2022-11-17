2b4a7bd3bd84eb0b5816fb2a536af11e39564596
hahmed1523 <hahmed1523@gmail.com>
2022-11-17 15:34:32 -0500

    Format date in comments messages

--- frontend/src/components/comments.js
+++ frontend/src/components/comments.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Comment = ({comment}) => {
+
+    const formatDate = com_date => {
+        const date = new Date(com_date);
+        return date.toDateString();
+
+    };
+
     return (
         <li>
             <p>{ comment.body }</p>
             <p>{ comment.username }</p>
-            <p>{ comment.updated_date }</p>
+            <p>{ formatDate(comment.updated_date) }</p>
         </li>
     );
 };
